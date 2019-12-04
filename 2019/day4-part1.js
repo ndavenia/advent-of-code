5ea4fda46b9a8d2831deb81fb38094c4b1438be7
ndavenia <58521780+ndavenia@users.noreply.github.com>
2019-12-04 16:28:16 +0100

    Create day4-part1.js

---
+++ 2019/day4-part1.js
@@ -0,0 +1,19 @@
+const isOrderedDigit = code =>
+   !code.some((digit, index) => parseInt(digit) < parseInt(code[index - 1] || 0));
+
+const atLeastOneDouble = code =>
+   code.some((digit, index) => digit === code[index - 1]);
+
+const isValid = code => isOrderedDigit(code) && atLeastOneDouble(code);
+
+const solveDay3 = (start, stop) => {
+  let nb = 0;
+  for(let i = start; i <= stop; i++) {
+    if(isValid([...("" + i)])) {
+      nb++;
+    }
+  }
+  return nb;
+};
+
+console.log(solveDay3(183564,657474));
